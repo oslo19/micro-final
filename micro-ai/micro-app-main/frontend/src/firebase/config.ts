6fa1844863b39f67e5b82740302c5e86d3e7fcff
Genard <119303896+oslo19@users.noreply.github.com>
2024-12-29 12:20:23 +0800

    Update config.ts

--- micro-ai/micro-app-main/frontend/src/firebase/config.ts
+++ micro-ai/micro-app-main/frontend/src/firebase/config.ts
@@ -1,13 +1,13 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA5mjFUZbFzcLzInqBXSB-LILWKrOSQ_4A",
-  authDomain: "micro-ai-5df30.firebaseapp.com",
-  projectId: "micro-ai-5df30",
-  storageBucket: "micro-ai-5df30.firebasestorage.app",
-  messagingSenderId: "290991587661",
-  appId: "1:290991587661:web:9b58158b4af3a19b107704"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
